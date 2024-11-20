9600209490c202c7d10c1fbfa9168ae7f9c2e897
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-11-20 18:37:13 -0300

    Refactor paso a C++

---
+++ simulador_fabrica/hebras.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <cstdlib>
+#include <unistd.h>
+#include <fstream>
+
+class ColaCircularDinamica {
+private:
+    std::vector<int> buffer;
+    size_t front;
+    size_t rear;
+    size_t count;
+    std::mutex mtx;
+    std::condition_variable cv_prod;
+    std::condition_variable cv_cons;
+    std::ofstream logFile;
+
+    void escribirLog(const std::string &mensaje) {
+        logFile << mensaje << std::endl;
+    }
+
+    void duplicar() {
+        std::string mensaje = "Duplicando tamaño del buffer... Tamaño actual: " + std::to_string(buffer.size());
+        escribirLog(mensaje);
+        size_t nuevoTam = buffer.size() * 2;
+        std::vector<int> nuevoBuffer(nuevoTam);
+
+        for (size_t i = 0; i < count; ++i) {
+            nuevoBuffer[i] = buffer[(front + i) % buffer.size()];
+        }
+
+        buffer = std::move(nuevoBuffer);
+        front = 0;
+        rear = count;
+        mensaje = "Nuevo tamaño del buffer: " + std::to_string(buffer.size());
+        escribirLog(mensaje);
+    }
+
+    void reducir() {
+        if (buffer.size() > 1) {
+            std::string mensaje = "Reduciendo tamaño del buffer... Tamaño actual: " + std::to_string(buffer.size());
+            escribirLog(mensaje);
+            size_t nuevoTam = buffer.size() / 2;
+            std::vector<int> nuevoBuffer(nuevoTam);
+
+            for (size_t i = 0; i < count; ++i) {
+                nuevoBuffer[i] = buffer[(front + i) % buffer.size()];
+            }
+
+            buffer = std::move(nuevoBuffer);
+            front = 0;
+            rear = count;
+            mensaje = "Nuevo tamaño del buffer: " + std::to_string(buffer.size());
+            escribirLog(mensaje);
+        }
+    }
+
+public:
+    ColaCircularDinamica(size_t tam_inicial) 
+        : buffer(tam_inicial), front(0), rear(0), count(0) {
+        logFile.open("log.txt", std::ios::out);
+        if (!logFile) {
+            std::cerr << "No se pudo abrir el archivo log.txt" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    ~ColaCircularDinamica() {
+        if (logFile.is_open()) {
+            logFile.close();
+        }
+    }
+
+    void push(int dato) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv_prod.wait(lock, [this] { return count < buffer.size(); });
+
+        buffer[rear] = dato;
+        rear = (rear + 1) % buffer.size();
+        ++count;
+
+        std::string mensaje = "Producido: " + std::to_string(dato) + " | Tamaño actual: " + std::to_string(count);
+        escribirLog(mensaje);
+
+        if (count == buffer.size()) {
+            duplicar();
+        }
+
+        cv_cons.notify_one();
+    }
+
+    int pop() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv_cons.wait(lock, [this] { return count > 0; });
+
+        int dato = buffer[front];
+        front = (front + 1) % buffer.size();
+        --count;
+
+        std::string mensaje = "Consumido: " + std::to_string(dato) + " | Tamaño actual: " + std::to_string(count);
+        escribirLog(mensaje);
+
+        if (count > 0 && count <= buffer.size() / 4) {
+            reducir();
+        }
+
+        cv_prod.notify_one();
+        return dato;
+    }
+
+    size_t capacidad() {
+        std::unique_lock<std::mutex> lock(mtx);
+        return buffer.size();
+    }
+};
+
+void productor(ColaCircularDinamica &cola, int id) {
+    for (int i = 0; i < 5; ++i) {
+        cola.push(i + id * 100);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void consumidor(ColaCircularDinamica &cola, int id, int tiempoEsperaMax) {
+    for (int i = 0; i < 20; ++i) {
+        int dato = cola.pop();
+        std::this_thread::sleep_for(std::chrono::seconds(tiempoEsperaMax));
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int productores = 0, consumidores = 0, tam_inicial = 0, tiempo_espera = 0;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "p:c:s:t:")) != -1) {
+        switch (opt) {
+            case 'p':
+                productores = std::atoi(optarg);
+                break;
+            case 'c':
+                consumidores = std::atoi(optarg);
+                break;
+            case 's':
+                tam_inicial = std::atoi(optarg);
+                break;
+            case 't':
+                tiempo_espera = std::atoi(optarg);
+                break;
+            default:
+                std::cerr << "Uso: " << argv[0] << " -p productores -c consumidores -s tamaño_inicial -t tiempo_espera" << std::endl;
+                return EXIT_FAILURE;
+        }
+    }
+
+    std::cout << "Productores =  " << productores << std::endl << "Consumidores = " << consumidores << std::endl << "Tamaño buffer = " << tam_inicial << std::endl << "Tiempo espera = " << tiempo_espera << std::endl;
+
+    if (productores <= 0 || consumidores <= 0 || tam_inicial <= 0 || tiempo_espera <= 0) {
+        std::cerr << "Todos los parámetros deben ser positivos y mayores a cero." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    ColaCircularDinamica cola(tam_inicial);
+
+    std::vector<std::thread> hilos;
+    for (int i = 0; i < productores; ++i) {
+        hilos.emplace_back(productor, std::ref(cola), i + 1);
+    }
+
+    for (int i = 0; i < consumidores; ++i) {
+        hilos.emplace_back(consumidor, std::ref(cola), i + 1, tiempo_espera);
+    }
+
+    for (auto &hilo : hilos) {
+        hilo.join();
+    }
+
+    return 0;
+}
